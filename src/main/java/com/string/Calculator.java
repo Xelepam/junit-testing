4089e55c10df2d6e226aaa626144acef1344835f
Omar Enrique Castro <oecastrom@gmail.com>
2018-11-30 16:58:16 -0600

    finished calculator testing

--- src/main/java/com/string/Calculator.java
+++ src/main/java/com/string/Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
     }
 
     public int perform(int i, int j) {
-        return service.add(i,j)*i;
+        //return service.add(i,j)*i;
+        return (i+j)*i;
     }
 }
--- src/test/java/com/string/CalculatorTest.java
+++ src/test/java/com/string/CalculatorTest.java
@@ -3,14 +3,25 @@ package com.string;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
 import static  org.mockito.Mockito.*;
 
 
 public class CalculatorTest {
+
     Calculator c = null;
+
+    @Mock
     CalculatorService service = Mockito.mock(CalculatorService.class);
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
 
     @Before
     public void setUp() {
@@ -21,5 +32,6 @@ public class CalculatorTest {
     public void testPerform() {
         when(service.add(2,3)).thenReturn(5);
         assertEquals(10, c.perform(2,3));
+        verify(service).add(2,3);
     }
 }
